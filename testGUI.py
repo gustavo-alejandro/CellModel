c18efa556a1be740b580a961cafb145920f2001f
gustavo-alejandro <gaospinaa@unal.edu.co>
2023-08-02 20:33:47 +1000

    plot updated

--- testGUI.py
+++ testGUI.py
@@ -4,6 +4,11 @@ import Global as Glob
 import BathProperties as Bath
 import ElectroChemistry as Electro
 import CellVoltage as Volt
+import numpy as np
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
 
 StateVector = [4.2, 0, 2.9]
 InputVector = [126000, 0, 0]
@@ -12,8 +17,6 @@ ConstVector = [10.3, 7, .3, 0, 963]
 ##Create GUI window
 root = tk.Tk()
 root.title("Aluminium Cell Model")
-# plot_frame = ttk.Frame(root)
-# plot_frame.grid(row=1, column=7, rowspan=6, padx=10, pady=10)
 
 # Variables to store the updated values
 C_Al2O3 = tk.DoubleVar(value=StateVector[0])
@@ -22,7 +25,6 @@ C_CaF2 = tk.DoubleVar(value=ConstVector[1])
 C_LiF = tk.DoubleVar(value=ConstVector[3])
 C_MgF2 = tk.DoubleVar(value=ConstVector[2])
 T_Bath = tk.DoubleVar(value=ConstVector[4])
-
 I_line = tk.DoubleVar(value=InputVector[0])
 ACD = tk.DoubleVar(value=StateVector[2])
 
@@ -78,17 +80,17 @@ bath_cond_label.grid(row=0, column=0, padx=5, pady=5)
 cell_frame = ttk.LabelFrame(root, text="Cell input data")
 cell_frame.grid(row=0, column=0, padx=5, pady=10, sticky="w")
 
-ttk.Label(cell_frame, text="Current", width=10).grid(row=0, column=0, padx=5, pady=5)
+ttk.Label(cell_frame, text="Current [A]", width=10).grid(row=0, column=0, padx=5, pady=5)
 cell_current_entry = ttk.Entry(cell_frame, textvariable=I_line, width=6)
 cell_current_entry.grid(row=0, column=1, padx=1, pady=1)
 
-ttk.Label(cell_frame, text="ACD", width=10).grid(row=1, column=0, padx=5, pady=5)
+ttk.Label(cell_frame, text="ACD [cm]", width=10).grid(row=1, column=0, padx=5, pady=5)
 ACD_entry = ttk.Entry(cell_frame, textvariable=ACD, width=6)
 ACD_entry.grid(row=1, column=1, padx=1, pady=1)
 
 #Output
 V_cell_output_col = 6
-V_cell_frame = ttk.LabelFrame(root, text="V_cell")
+V_cell_frame = ttk.LabelFrame(root, text="V_cell [V]")
 V_cell_frame.grid(row=0, column=V_cell_output_col, padx=10, pady=10, sticky="w")
 
 V_cell_label = ttk.Label(V_cell_frame, text="V_cell: ")
@@ -194,7 +196,107 @@ def update_values():
     V_ext = InputVector[0]*Glob.Rext
     V_ext_label.config(text=f"V_ext: {V_ext:.4f}")
 
+
+def plot_cell_volt():
+
+    # Define a range of C_Al2O3 values from 2 to 8
+    C_Al2O3_values = np.linspace(1.9, 8, 100)
+    C_AlF3 = ConstVector[0]
+    C_CaF2 = ConstVector[1]
+    C_LiF = ConstVector[3]
+    C_MgF2 = ConstVector[2]
+    T_Bath = ConstVector[4]
+    I_line = InputVector[0]
+    ACD = StateVector[2]
+
+    # Initialize empty lists to store the calculated values
+    Erev_values = []
+    Eca_values = []
+    Esa_values = []
+    Ecc_values = []
+    Vbath_values = []
+    Vbub_values = []
+    V_an_values = []
+    V_ca_values = []
+    V_ext_values = []
+    V_cell_values = []
+
+
+
+    for C_Al2O3 in C_Al2O3_values:
+        BR = Bath.Ratio(C_Al2O3, C_AlF3, C_CaF2, C_LiF, C_MgF2)[0]
+        Erev=Electro.ReversiblePotential(C_Al2O3, C_AlF3, C_CaF2, C_LiF, C_MgF2, T_Bath)[0]
+        Erev_values.append(Electro.ReversiblePotential(C_Al2O3, C_AlF3, C_CaF2, C_LiF, C_MgF2, T_Bath)[0])
+        Esa_values.append(Electro.AnodeSurfOverVolt(C_Al2O3, T_Bath, I_line)[0])
+        BCover = Electro.BubbleCoverage(C_Al2O3, BR, I_line)[0]
+        Vbub_values.append(I_line * Electro.BubbleRes(BCover, Electro.BubbleThickness(I_line),
+                                                      Bath.Conductivity(C_Al2O3, C_AlF3, C_CaF2, C_LiF, C_MgF2, T_Bath)[
+                                                          0]))
+
+        Eca_values.append(Electro.AnodeConcOverVolt(C_Al2O3, T_Bath, I_line)[0])
+        Ecc_values.append(Electro.CathodeConcOverVolt(BR, T_Bath, I_line)[0])
+
+        dB = Electro.BubbleThickness(I_line)
+        kbath = Bath.Conductivity(C_Al2O3, ConstVector[0], ConstVector[1], ConstVector[3], ConstVector[2],
+                                  ConstVector[4])[0]
+        Rbath = Electro.BathRes(ACD, dB, kbath)
+        Vbath_values.append(I_line * Rbath)
+
+        V_ca_values.append(I_line * Glob.Ran)
+        V_an_values.append(I_line * Glob.Rca)
+        V_ext_values.append(I_line * Glob.Rext)
+
+        #V_cell_values.append
+
+    # Create a figure and axis for the plot
+    fig = Figure(figsize=(8, 6))
+    ax = fig.add_subplot(111)
+    # Plot the calculated values against C_Al2O3
+    ax.plot(C_Al2O3_values, Erev_values, label="E_rev")
+    ax.plot(C_Al2O3_values, Esa_values, label="E_sa")
+    ax.plot(C_Al2O3_values, Vbub_values, label="V_bub")
+    ax.plot(C_Al2O3_values, Eca_values, label="E_ca")
+    ax.plot(C_Al2O3_values, Ecc_values, label="E_cc")
+    ax.plot(C_Al2O3_values, Vbath_values, label="V_bath")
+    ax.plot(C_Al2O3_values, Vbub_values, label="V_bub")
+    ax.plot(C_Al2O3_values, V_ca_values, label="V_ca")
+    ax.plot(C_Al2O3_values, V_an_values, label="V_an")
+    ax.plot(C_Al2O3_values, V_ext_values, label="V_ext")
+
+
+
+    # Set labels and legend
+    ax.set_xlabel("C_Al2O3")
+    ax.set_ylabel("Voltage")
+    ax.legend()
+    return fig
+
+def plot_button_cmd():
+    #    # Call the plot_cell_volt function to get the figure
+    fig = plot_cell_volt()
+
+    # Create a new frame for the plot
+    plot_frame = ttk.LabelFrame(root, text="Plot")
+    plot_frame.grid(row=0, column=8, columnspan=2, pady=10)
+
+    # Create a canvas for the plot
+    canvas = FigureCanvasTkAgg(fig, master=plot_frame)
+    canvas.draw()
+
+    # Display the plot in the new frame
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    toolbar = NavigationToolbar2Tk(canvas, plot_frame)
+    toolbar.update()
+    canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+
+
+
 update_button = ttk.Button(root, text="Update Values", command=update_values)
 update_button.grid(row=len(slider_labels), column=0, columnspan=2, pady=10)
+
+# Modify the command for the "Plot" button to call the plot_button_cmd function
+plot_button = ttk.Button(root, text="Plot", command=plot_button_cmd)
+plot_button.grid(row=len(slider_labels), column=1, columnspan=2, pady=10)
 
 root.mainloop()
